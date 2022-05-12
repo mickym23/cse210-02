977bfb64657b0c50c35f21df9973bae8ebd39420
simycodes <simycodes@gmail.com>
2022-05-12 11:57:57 +0200

    Hilo game instructions code,all program comments,improved game display

--- HiLo/Card.py
+++ HiLo/Card.py
@@ -1,10 +1,26 @@
 import random
 
 class Card:
+    """A single card with a number.
+    The card holds the randomly picked number.
+   
+    Attributes:
+        value (int): The actual card number.
+    """
 
     def __init__(self):
+        """Constructs a new instance of the Card.
+
+        Args:
+            self (Card): An instance of Card.
+        """
         self.value = 0
-        self.points = 0
+        # self.points = 0
 
     def pick(self):
+        """Picks a randome number between 1 and 13.
+        
+        Args:
+            self (Card): An instance of Card.
+        """
         self.value = random.randint(1, 13)
--- HiLo/Player.py
+++ HiLo/Player.py
@@ -1,16 +1,36 @@
+from pyray import poll_input_events
 from Card import Card
 
 class Player:
+    """The person directing the game flow. 
+     Controls the sequence of play.
+
+    Attributes:
+        is_playing (boolean): indicates if the game is being played or not.
+        score (int): The score for one round of play.
+        total_score (int): Accumulated total score - player starts with an initial 300 points.
+        higher_lower :holds user input for the guess of number being higher or lower
+    """
 
     def __init__(self):
-        self.card = ''
+        """Constructs a new Player.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
         self.is_playing = True
+        # self.card = ''
         self.score = 0
         self.total_score = 300
         self.player_current_card = ''
         self.higher_lower = ''
 
     def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
         while self.is_playing:
             self.get_inputs()
             self.do_updates()
@@ -18,13 +38,26 @@ class Player:
             self.player_is_playing()
 
     def get_inputs(self):
+        """Ask the user the guess of the next card being higher or lower.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         current_card = Card()
         current_card.pick()
-        print(f'The card is {current_card.value}')
-        self.higher_lower = input("Higher or lower [h/l] ")
+
+        print("\nHiLO Game!")
+        print("Guess if the next card number will be Higher or Lower than the current card number shown.")
+        print(f'\nThe card is {current_card.value}')
+        self.higher_lower = input("Higher or lower [h/l] : ")
         self.player_current_card = current_card.value
 
     def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Player): An instance of Player.
+        """
        
         if not self.is_playing:
             return 
@@ -32,9 +65,12 @@ class Player:
         player_new_card = Card()
         player_new_card.pick()
 
-        # if self.player_current_card > player_new_card.value:
+        # if self.player_current_card > player_new_card.value or if if self.player_current_card < player_new_card.value
+        # show the player if their guess was right or wrong
         print(f'Next card was: {player_new_card.value}')
 
+        # if player guess is higher and its true add 100 to player score else reduce by 75
+        # if player is lower and its true add 100 to player score else reduce by 75
         if self.higher_lower == "h" and self.player_current_card < player_new_card.value:
             self.score = self.score + 100
         elif self.higher_lower == "h" and self.player_current_card > player_new_card.value:
@@ -44,11 +80,23 @@ class Player:
         elif self.higher_lower == "l" and self.player_current_card < player_new_card.value:
             self.score = self.score - 75
 
+
         self.total_score += self.score
+
     def do_outputs(self):
+        """Shows the players total score which is either increased if player won or reduced if player lost. 
+
+        Args:
+            self (Player): An instance of Player.
+        """
         print(f'Your score: {self.total_score}')
         self.score = 0
 
     def player_is_playing(self):
-        play_again = input("Play again? [y/n] ")
+        """Asks the player if he wants to play again or not. 
+
+        Args:
+            self (Player): An instance of Player.
+        """
+        play_again = input("Play again? [y/n] : ")
         self.is_playing = (play_again == "y")
